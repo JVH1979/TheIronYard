4ff18633c5f71ad78cb8d0171a6ed40f992cad3c
John Hammond <johnhammond1979@gmail.com>
2014-08-04 02:08:44 -0400

    blog homework

--- blog/blog.rb
+++ blog/blog.rb
@@ -1,11 +1,49 @@
-require 'rubygems'
 require 'sinatra'
+require 'data_mapper'
 
-get '/form' do 
-	erb :form
-	
+DataMapper.setup
+(:default, 'mysql://root@root/blog_posts')
+
+class Blog
+	include DataMapper: :Resource
+		property :id, Serial
+		property :blog_title, String
+		property :Blog_entries, String
 end
 
-post '/form' do
-  "You said '#{params[:message]}'"
+DataMapper.finalize.auto_upgrade!
+
+get '/' do
+	@blog = blog.all
+	erb :create_blogpost, layout: :blog_layout
+end
+
+get '/create_blogpost' do
+	erb :create_blogpost, layout: :blog_layout
+end
+
+post '/create_blogpost' do
+	@blog = Blog.create params[:blog]
+	redirect to ('/')
+end
+
+get '/display_blogpost' do
+	@blog = Blog.get params[:blog]
+	erb :display_blogpost, layout: :blog_layout
+end
+
+get "/edit_blogpost" do
+	@blog = Blog.get params[:blog]
+	erb :edit_blogpost, layout: :blog_layout
+end
+
+put 'edit_blogpost' do
+	p params
+	@blog = Blog.get params[:blog]
+end
+
+delete 'delete_blogpost' do
+	@blog = Blog.get params[:blog]
+	@blog.destroy
+	redirect to("/")
 end
