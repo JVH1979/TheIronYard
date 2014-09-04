58300f3e617fcd486be5901faa03f8fa96fde4c5
John Hammond <johnhammond1979@gmail.com>
2014-09-04 07:25:03 -0400

    feature testing

--- stjohn_hospitalv3/spec/rails_helper.rb
+++ stjohn_hospitalv3/spec/rails_helper.rb
@@ -3,6 +3,16 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'selenium/webdriver'
+require 'shoulda-matchers'
+require 'capybara'
+
+
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+Capybara.javascript_driver = :chrome
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
